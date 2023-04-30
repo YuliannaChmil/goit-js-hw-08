c21622ad83d0a586e992715ae15f902a8f5119c2
Yuliia-Yul <yulianna.ego@gmail.com>
2023-04-30 16:33:19 +0300

    finished task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,43 @@
+import throttle from 'lodash.throttle';
 
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  input: document.querySelector('input'),
+  textarea: document.querySelector('textarea'),
+};
+
+const STORAGE_KEY = 'feedback-form-state';
+let formData = {};
+
+refs.form.addEventListener('input', throttle(inputData, 500));
+refs.form.addEventListener('submit', onSubmit);
+
+resultFeedback();
+
+function inputData(event) {
+  formData = {
+    email: refs.input.value.trim(),
+    message: refs.textarea.value.trim(),
+  };
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function onSubmit(event) {
+  event.preventDefault();
+  const { email, message } = event.currentTarget.elements;
+  console.log({ email: email.value.trim(), message: message.value.trim() });
+  if (email.value === '' || message.value === '') {
+    return alert('Заповніть всі поля!');
+  }
+  localStorage.removeItem(STORAGE_KEY);
+  event.currentTarget.reset();
+  dataForm = {};
+}
+
+function resultFeedback() {
+  let data = localStorage.getItem(STORAGE_KEY);
+  if (!data) return;
+  formData = JSON.parse(data);
+  refs.input.value = formData.email ?? '';
+  refs.textarea.value = formData.message ?? '';
+}
